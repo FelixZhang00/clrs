b6852f9e269833296401a5a8e06e4d0970440572
Jamffy <l836828946@gmail.com>
2017-10-19 12:50:10 +0800

    2.1-2

---
+++ Chapter_02_Getting_Started/2.1-2.py
@@ -0,0 +1,41 @@
+import random
+import unittest
+
+
+def insertion_sort(a):
+  for i in range(1, len(a)):
+    key = a[i]
+    j = i - 1
+    while j >= 0 and a[j] < key:
+      a[j + 1] = a[j]
+      j -= 1
+    a[j + 1] = key
+
+
+def simpleTest():
+  a = [5, 2, 4, 6, 1, 3]
+
+  print "before sort:"
+  print a
+  insertion_sort(a)
+  print "after sort:"
+  print a
+
+
+class InsertSortTestCase(unittest.TestCase):
+  def random_array(self):
+    return [random.randint(0, 100) for _ in range(random.randint(1, 100))]
+
+  def test_random(self):
+    for _ in range(1000):
+      arr = self.random_array()
+      sorted_arr = sorted(arr)
+      sorted_arr.reverse()
+      insertion_sort(arr)
+      self.assertEqual(arr, sorted_arr)
+
+
+if __name__ == '__main__':
+  # simpleTest()
+
+  unittest.main()
