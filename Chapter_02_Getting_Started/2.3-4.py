594adfd949a67c7ec7c274bacf5fd4ebceeb17e6
Jamffy <l836828946@gmail.com>
2017-10-22 10:08:04 +0800

    2.3-4

--- Chapter_02_Getting_Started/2.3-4.py
+++ Chapter_02_Getting_Started/2.3-4.py
@@ -6,8 +6,26 @@
 '''
 
 
+# 把n位置所在元素的值插入到arr[0,n-1]
+def insert(arr, n):
+  key = arr[n]
+  j = n - 1
+  while j >= 0 and arr[j] > key:
+    arr[j + 1] = arr[j]
+    j -= 1
+  arr[j+1] = key
+
+
+def insert_sort_interal(arr, n):
+  if n == 0:
+    return
+  insert_sort_interal(arr, n - 1)
+  insert(arr, n)
+
+
 def insert_sort(arr):
-  pass
+  insert_sort_interal(arr, len(arr) - 1)
+
 
 def simpleTest():
   a = [5, 2, 4, 6, 1, 3]
@@ -18,6 +36,20 @@ def simpleTest():
   print "after sort:"
   print a
 
+import random
+import unittest
+
+class InsertSortTestCase(unittest.TestCase):
+  def random_array(self):
+    return [random.randint(0,100) for _ in range(random.randint(1,100))]
+
+  def test_random(self):
+    for _ in range(1000):
+      arr = self.random_array()
+      sorted_arr = sorted(arr)
+      insert_sort(arr)
+      self.assertEqual(arr,sorted_arr)
+
 if __name__ == '__main__':
-  simpleTest()
-  # unittest.main()
+  # simpleTest()
+  unittest.main()
